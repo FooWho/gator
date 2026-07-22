797c1cec3adc9ef1ab1883206a72949e36d9fbca
Jason Elison <jason.elison@gmail.com>
2026-07-21 22:41:13 -0400

    Cleanup

--- cmd/gator/main.go
+++ cmd/gator/main.go
@@ -8,24 +8,29 @@ import (
 )
 
 func main() {
-
-	gatorState := state{}
+	cfg := &config.Config{}
+	if err := cfg.Read(); err != nil {
+		fmt.Printf("error reading config: %s\n", err)
+		os.Exit(1)
+	}
+	gatorState := state{config: cfg}
 	gatorCommands := commands{}
-	gatorState.config = &config.Config{}
-	gatorState.config.Read()
-	gatorCommands.cmdHandler = make(map[string]func(*state, command) error)
-	gatorCommands.cmdHandler["login"] = handlerLogin
+	gatorCommands.register("login", handlerLogin)
 
 	args := os.Args
 	if len(args) < 2 {
 		fmt.Print("gator requires a command as an argument\n")
 		os.Exit(1)
-	} else {
-		cmd := command{name: args[1], args: args[2:]}
-		fmt.Printf("Got command:\n")
-		fmt.Printf("   name: %s\n", cmd.name)
-		fmt.Printf("   args[0]: %v\n", cmd.args[0])
-		gatorCommands.cmdHandler[cmd.name](&gatorState, cmd)
 	}
 
+	cmd := command{name: args[1], args: args[2:]}
+	fmt.Printf("Got command:\n")
+	fmt.Printf("   name: %s\n", cmd.name)
+	if len(cmd.args) > 0 {
+		fmt.Printf("   args[0]: %v\n", cmd.args[0])
+	}
+	if err := gatorCommands.run(&gatorState, cmd); err != nil {
+		fmt.Printf("error running command: %s\n", err)
+		os.Exit(1)
+	}
 }
